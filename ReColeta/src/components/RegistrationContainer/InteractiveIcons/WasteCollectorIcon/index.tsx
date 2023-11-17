708e8291a4c6a70d9d70551219bfc3f54c02e9e1
wakenedo <a_nuner@outlook.com>
2023-11-17 16:43:05 -0300

    Seleção de Função
    Mobile:
    Efeito active funcionando corretamente
    
    Desktop:
    Efeito hover funcionando corretamente.
    Efeito active não está funcionando corretamente(Verificar)

--- ReColeta/src/components/RegistrationContainer/InteractiveIcons/WasteCollectorIcon/index.tsx
+++ ReColeta/src/components/RegistrationContainer/InteractiveIcons/WasteCollectorIcon/index.tsx
@@ -1,16 +1,44 @@
+import { useState } from 'react'
 import '../styles/InteractiveIcons.css'
+import WasteCollectorIconDefault from '../../../../assets/img/RegisterArea/RecyclingTruck.png'
+import WasteCollectorIconActive from '../../../../assets/img/RegisterArea/RecyclingTruckWhite.png'
 
 interface IconProps {
   onSelect: (role: string) => void;
 }
 
 export const WasteCollectorIcon: React.FC<IconProps> = ({ onSelect }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsActive(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsActive(false);
+  };
+
+  const handleClick = () => {
+    setIsActive(true);
+    onSelect('ROLE_USER');
+  };
+
   return (
-    <div className="icon-container-waste-producer" onClick={() => onSelect('ROLE_USER')}>
-      <p className="icon-title-waste-producer">Produtor de Resíduos</p>
-      <img className="icon-image-waste-producer" src="" alt="" />
-      <p className="icon-text-description-waste-producer">
-        O usuário produtor de resíduos deve completar o cadastro para que possa ter acesso à plataforma e registrar seus resíduos.
+    <div
+      className={`icon-container-waste-collector ${isActive ? 'active' : ''}`}
+      onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <p className="icon-title-waste-collector">Coletor de Resíduos</p>
+      <img
+        className="icon-image-waste-collector"
+        src={isActive ? WasteCollectorIconActive : WasteCollectorIconDefault}
+        alt={isActive ? 'wasteCollectorActive' : 'wasteCollectorDefault'}
+      />
+
+      <p className="icon-text-description-waste-collector">
+        O usuário coletor de resíduos pode buscar por coletas próximas à sua localização e começar a coletar resíduos.
       </p>
     </div>
   );
--- ReColeta/src/components/RegistrationContainer/InteractiveIcons/WasteProducerIcon/index.tsx
+++ ReColeta/src/components/RegistrationContainer/InteractiveIcons/WasteProducerIcon/index.tsx
@@ -1,19 +1,45 @@
-import '../styles/InteractiveIcons.css';
+import { useState } from 'react';
+import WasteProducerIconDefault from '../../../../assets/img/RegisterArea/oilTower.png';
+import WasteProducerIconActive from '../../../../assets/img/RegisterArea/oilTowerWhite.png';
 
 interface IconProps {
   onSelect: (role: string) => void;
 }
 
-
-
 export const WasteProducerIcon: React.FC<IconProps> = ({ onSelect }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsActive(true);
+  };
+
+
+  const handleMouseLeave = () => {
+    setIsActive(false);
+  };
+
+  const handleClick = () => {
+    setIsActive(true);
+    onSelect('ROLE_USER');
+  };
+
   return (
-    <div className="icon-container-waste-collector" onClick={() => onSelect('ROLE_USER')}>
-      <p className="icon-title-waste-collector">Coletor de Resíduos</p>
-      <img className="icon-image-waste-collector" src="" alt="" />
-      <p className="icon-text-description-waste-collector">
-        O usuário coletor de resíduos pode buscar por coletas próximas à sua localização e começar a coletar resíduos.
+    <div
+      className={`icon-container-waste-producer ${isActive ? 'active' : ''}`}
+      onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <p className="icon-title-waste-producer">Produtor de Resíduos</p>
+      <img
+        className="icon-image-waste-producer"
+        src={isActive ? WasteProducerIconActive : WasteProducerIconDefault}
+        alt={isActive ? 'wasteProducerActive' : 'wasteProducerDefault'}
+      />
+      <p className="icon-text-description-waste-producer">
+        O usuário produtor de resíduos deve completar o cadastro para que possa ter acesso à plataforma e registrar seus resíduos.
       </p>
     </div>
   );
 };
+
